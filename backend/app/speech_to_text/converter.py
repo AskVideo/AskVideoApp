818cca4111c46e334f6e440b3e47fdbb25a7090e
tahsineneskuru <teneskuru@icloud.com>
2024-02-09 23:28:21 +0300

    implemented youtube search and download

--- backend/app/speech_to_text/converter.py
+++ backend/app/speech_to_text/converter.py
@@ -1,5 +1,6 @@
 import whisper
 from pytube import YouTube, Search
+import logging
 
 class Converter:
     def __init__(self):
@@ -9,5 +10,22 @@ class Converter:
     def text_from_uploaded_file(self, video_path):
         result = self.model.transcribe(video_path)
 
-    def search_yt(self, query):
-        pass
+    def search_yt(self, query, k=10):
+        searchResults = []
+        s = Search(query)
+        while(len(s.results) < k):
+            s.get_next_results()
+
+        for v in s.results[0:k]:
+            searchResults.append({'title':v.title,'url': v.watch_url})
+        return searchResults
+
+    def Download(self,url):
+        youtubeObject = YouTube(url)
+        youtubeObject = youtubeObject.streams.filter(res="360p").first()
+        try:
+            youtubeObject.download()
+        except Exception as e:
+            logging.error(e)
+            return
+        logging.info("Download is completed successfully")
