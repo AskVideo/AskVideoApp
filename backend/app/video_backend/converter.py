890689670d3ac26a0b81dc5138a8cd52e479cceb
Onur1258 <onur.deniz_1999@hotmail.com>
2024-02-12 16:50:29 +0300

    qdrant initilazed

--- backend/app/video_backend/converter.py
+++ backend/app/video_backend/converter.py
@@ -1,10 +1,25 @@
 import whisper
 from pytube import YouTube, Search
 import logging
+from app.database.qdrant import QdrantDb
+from langchain.docstore.document import Document
+from langchain.text_splitter import CharacterTextSplitter
 
 class Converter:
     def __init__(self):
         self.model = whisper.load_model("base")
+        self.qdrant = QdrantDb(cloud_api_key='qd2EJwwKcPCH6H5Q3QIqMZD8goCahUNMAFCx2Zbk2HR98lnJaPrUTg', openai_api_key="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")
+
+    # for test
+    def insert_docs(self):
+        txt = "Random words are really good"
+        doc =  [Document(page_content=txt, metadata={"source": "local"})]
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        docs = text_splitter.split_documents(doc)
+        self.qdrant.instert_docs(docs)
+        
+        docs = self.qdrant.search_top_k("words are good")
+        return docs
 
 
     def text_from_uploaded_file(self, video_path):
