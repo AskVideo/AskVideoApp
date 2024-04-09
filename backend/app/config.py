ac045c654daf481769118cf61388008e66d79948
Onur1258 <onur.deniz_1999@hotmail.com>
2024-04-09 18:56:07 +0300

    db url

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI='postgresql://albcspehyxvqog:9385e7a03d2fea1ded09b1edbcb7944ba3b6aef0399148ed53554c796703e033@ec2-34-238-185-225.compute-1.amazonaws.com:5432/d320da5nmnkl9o'
+    SQLALCHEMY_DATABASE_URI='postgresql://ckvzywkagoddxr:618f2eabb38418290e0644af46e1cc0e8f49a10e1390b70e40ec9a0cc16340b4@ec2-35-170-27-172.compute-1.amazonaws.com:5432/d6egpaeus70t96'
     SECRET_KEY='8c06d9793416fcfcd7841d71'
